9aace6f1b38722e6bfc93681f7771475fb965846
jiashunx <wenjiashun@163.com>
2024-05-11 23:37:25 +0800

    :white_check_mark: 完善测试用例

--- client/client_test.go
+++ client/client_test.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-    "fmt"
     "github.com/jiashunx/eureka-client-go/meta"
     "github.com/stretchr/testify/assert"
     "testing"
@@ -13,10 +12,12 @@ var serviceUrl = "http://admin:123123@127.0.0.1:20000/eureka,http://192.168.138.
 // TestEurekaClient1 客户端测试样例1
 func TestEurekaClient1(t *testing.T) {
     ast := assert.New(t)
+
+    // 创建客户端
     client, err := NewEurekaClient(&meta.EurekaConfig{
         InstanceConfig: &meta.InstanceConfig{
-            AppName:       "test-eureka-client",
-            InstanceId:    "127.0.0.1:8081",
+            AppName:       "eureka-client-test1",
+            InstanceId:    "127.0.0.1:8080",
             NonSecurePort: 8081,
             Hostname:      "127.0.0.1",
         },
@@ -24,31 +25,60 @@ func TestEurekaClient1(t *testing.T) {
             ServiceUrlOfDefaultZone: serviceUrl,
         },
     })
-    ast.Nilf(err, "创建EurekaClient失败，失败原因：%v", err)
+    ast.Nilf(err, "%v", err)
+
     // 启动客户端
     err = client.Start()
-    ast.Nilf(err, "启动EurekaClient失败，失败原因：%v", err)
+    ast.Nilf(err, "%v", err)
+
+    <-time.NewTimer(time.Second).C
+
     // client默认注册时实例状态为STARTING，需手工修改状态为UP
     response := client.ChangeStatus(meta.StatusUp)
-    ast.Nilf(response.Error, "更新服务实例状态为UP，请求失败，失败原因：%v", response.Error)
+    ast.Nilf(response.Error, "%v", response.Error)
+
+    <-time.NewTimer(time.Second).C
+
     // 停止客户端，停止后客户端不可用，服务注册与发现相关goroutine自动停止并回收
     response = client.Stop()
-    ast.Nilf(response.Error, "停止EurekaClient失败，失败原因：%v", response.Error)
-    <-time.NewTimer(2 * time.Second).C
+    ast.Nilf(response.Error, "%v", response.Error)
+
+    <-time.NewTimer(time.Second).C
 
     // 停止客户端后可再次启动客户端
     err = client.Start()
-    ast.Nilf(err, "再次启动EurekaClient失败，失败原因：%v", err)
+    ast.Nilf(err, "%v", err)
+
+    <-time.NewTimer(time.Second).C
+
     // 停止客户端
-    client.Stop()
+    response = client.Stop()
+    ast.Nilf(response.Error, "%v", response.Error)
 }
 
 // TestEurekaClient2 客户端测试样例2
 func TestEurekaClient2(t *testing.T) {
     ast := assert.New(t)
-    client, err := NewEurekaClient(&meta.EurekaConfig{
+
+    // 创建客户端21
+    client21, err := NewEurekaClient(&meta.EurekaConfig{
         InstanceConfig: &meta.InstanceConfig{
-            AppName:             "test-eureka-client",
+            AppName:             "eureka-client-test2",
+            InstanceId:          "127.0.0.1:8081",
+            NonSecurePort:       8081,
+            Hostname:            "127.0.0.1",
+            InstanceEnabledOnIt: &meta.True,
+        },
+        ClientConfig: &meta.ClientConfig{
+            ServiceUrlOfDefaultZone: serviceUrl,
+        },
+    })
+    ast.Nilf(err, "%v", err)
+
+    // 创建客户端22
+    client22, err := NewEurekaClient(&meta.EurekaConfig{
+        InstanceConfig: &meta.InstanceConfig{
+            AppName:             "eureka-client-test2",
             InstanceId:          "127.0.0.1:8082",
             SecurePort:          8082,
             IpAddress:           "127.0.0.1",
@@ -56,8 +86,9 @@ func TestEurekaClient2(t *testing.T) {
             InstanceEnabledOnIt: &meta.True,
         },
         ClientConfig: &meta.ClientConfig{
-            Region: "cn",
-            Zone:   "zone2",
+            PreferSameZoneEureka: &meta.False,
+            Region:               "cn",
+            Zone:                 "zone1",
             AvailableZones: map[string]string{
                 "cn":  "zone1,zone2",
                 "usa": "zone3",
@@ -65,28 +96,48 @@ func TestEurekaClient2(t *testing.T) {
             },
             ServiceUrlOfAllZone: map[string]string{
                 "zone1": serviceUrl,
-                "zone2": serviceUrl,
+                "zone2": "",
             },
         },
     })
-    ast.Nilf(err, "创建EurekaClient失败，失败原因：%v", err)
+    ast.Nilf(err, "%v", err)
+
     // 启动客户端（指定了 InstanceEnabledOnIt 参数，默认注册时服务实例状态为UP）
-    err = client.Start()
-    ast.Nilf(err, "启动EurekaClient失败，失败原因：%v", err)
+    err = client21.Start()
+    ast.Nilf(err, "%v", err)
+    err = client22.Start()
+    ast.Nilf(err, "%v", err)
+
+    <-time.NewTimer(60 * time.Second).C
+
+    // 服务发现
+    app, err := client21.AccessApp(client21.config.AppName)
+    ast.Nilf(err, "%v", err)
+    ast.NotNil(app)
+
     // 停止客户端，停止后客户端不可用，服务注册与发现相关goroutine自动停止并回收
-    response := client.Stop()
-    ast.Nilf(response.Error, "停止EurekaClient失败，失败原因：%v", response.Error)
-    <-time.NewTimer(2 * time.Second).C
+    response := client21.Stop()
+    ast.Nilf(response.Error, "%v", response.Error)
+    response = client22.Stop()
+    ast.Nilf(response.Error, "%v", response.Error)
 }
 
 // TestEurekaClient3 客户端测试样例3
 func TestEurekaClient3(t *testing.T) {
     ast := assert.New(t)
+
+    // 创建客户端（未开启服务注册与服务发现功能）
     client, err := NewEurekaClient(&meta.EurekaConfig{})
-    ast.Nilf(err, "创建EurekaClient失败，失败原因：%v", err)
+    ast.Nilf(err, "%v", err)
+
     // 获取与eureka server通讯的http客户端
     httpClient := client.HttpClient
+
+    // 通过HttpClient与eureka server交互
     response := httpClient.QueryApps(&meta.EurekaServer{ServiceUrl: serviceUrl})
-    ast.Nilf(response.Error, "查询服务列表失败，失败原因：%v", response.Error)
-    fmt.Println("查询服务列表:", response.Apps)
+    ast.Nilf(response.Error, "%v", response.Error)
+    ast.True(len(response.Apps) > 0)
+
+    // 客户端无需关闭
+    // client.Stop()
 }
--- client/http_test.go
+++ client/http_test.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-    "fmt"
     "github.com/jiashunx/eureka-client-go/meta"
     "github.com/stretchr/testify/assert"
     "testing"
@@ -20,139 +19,99 @@ var eurekaServer2 = &meta.EurekaServer{
     Password:   "123123",
 }
 
-var instance = &meta.InstanceInfo{
-    AppName:    "test-http-client",
+var httpInstance = &meta.InstanceInfo{
+    AppName:    "http-client-test",
     InstanceId: "127.0.0.1:8080",
     IpAddr:     "127.0.0.1",
     SecurePort: &meta.PortWrapper{
         Enabled: meta.StrTrue,
         Port:    8080,
     },
-    VipAddress:       "test-http-client",
-    SecureVipAddress: "test-http-client",
+    VipAddress:       "http-client-test-A",
+    SecureVipAddress: "http-client-test-B",
 }
 
 var httpClient = &HttpClient{}
 
 func TestRegister(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleRegister(serviceUrl1, instance)
-    ast.Nilf(response.Error, "Register处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleRegister(serviceUrl1, httpInstance)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(204, response.StatusCode)
-    fmt.Println("Register请求方法:", response.Response.Request.Method)
-    fmt.Println("Register请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("Register请求报文:", response.Response.Request.Body)
-    fmt.Println("Register响应状态:", response.StatusCode)
 }
 
 func TestHeartbeat(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleHeartbeat(serviceUrl1, instance.AppName, instance.InstanceId)
-    ast.Nilf(response.Error, "Heartbeat处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleHeartbeat(serviceUrl1, httpInstance.AppName, httpInstance.InstanceId)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("Heartbeat请求方法:", response.Response.Request.Method)
-    fmt.Println("Heartbeat请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("Heartbeat响应状态:", response.StatusCode)
 }
 
 func TestQueryApps(t *testing.T) {
     ast := assert.New(t)
     response := httpClient.SimpleQueryApps(serviceUrl1)
-    ast.Nilf(response.Error, "QueryApps处理失败，失败原因：%s", response.Error)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("QueryApps请求方法:", response.Response.Request.Method)
-    fmt.Println("QueryApps请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("QueryApps响应状态:", response.StatusCode)
-    fmt.Println("QueryApps响应报文:", response.Response.Body)
-    fmt.Println("QueryApps返回结果:", fmt.Sprintf("%#v", *response.Apps[0]))
+    ast.True(len(response.Apps) > 0)
 }
 
 func TestQueryApp(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleQueryApp(serviceUrl1, instance.AppName)
-    ast.Nilf(response.Error, "QueryApp处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleQueryApp(serviceUrl1, httpInstance.AppName)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("QueryApp请求方法:", response.Response.Request.Method)
-    fmt.Println("QueryApp请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("QueryApp响应状态:", response.StatusCode)
-    fmt.Println("QueryApp响应报文:", response.Response.Body)
-    fmt.Println("QueryApp返回结果:", fmt.Sprintf("%#v", *response.Instances[0]))
+    ast.True(len(response.Instances) > 0)
 }
 
 func TestQueryAppInstance(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleQueryAppInstance(serviceUrl1, instance.AppName, instance.InstanceId)
-    ast.Nilf(response.Error, "QueryAppInstance处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleQueryAppInstance(serviceUrl1, httpInstance.AppName, httpInstance.InstanceId)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("QueryAppInstance请求方法:", response.Response.Request.Method)
-    fmt.Println("QueryAppInstance请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("QueryAppInstance响应状态:", response.StatusCode)
-    fmt.Println("QueryAppInstance响应报文:", response.Response.Body)
-    fmt.Println("QueryAppInstance返回结果:", fmt.Sprintf("%#v", *response.Instance))
+    ast.NotNil(response.Instance)
 }
 
 func TestQueryInstance(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleQueryInstance(serviceUrl1, instance.InstanceId)
-    ast.Nilf(response.Error, "QueryInstance处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleQueryInstance(serviceUrl1, httpInstance.InstanceId)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("QueryInstance请求方法:", response.Response.Request.Method)
-    fmt.Println("QueryInstance请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("QueryInstance响应状态:", response.StatusCode)
-    fmt.Println("QueryInstance响应报文:", response.Response.Body)
-    fmt.Println("QueryInstance返回结果:", fmt.Sprintf("%#v", *response.Instance))
+    ast.NotNil(response.Instance)
 }
 
 func TestChangeStatus(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleChangeStatus(serviceUrl1, instance.AppName, instance.InstanceId, meta.StatusOutOfService)
-    ast.Nilf(response.Error, "ChangeStatus处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleChangeStatus(serviceUrl1, httpInstance.AppName, httpInstance.InstanceId, meta.StatusOutOfService)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("ChangeStatus请求方法:", response.Response.Request.Method)
-    fmt.Println("ChangeStatus请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("ChangeStatus响应状态:", response.StatusCode)
 }
 
 func TestModifyMetadata(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleModifyMetadata(serviceUrl1, instance.AppName, instance.InstanceId, map[string]string{"hello": "world"})
-    ast.Nilf(response.Error, "ModifyMetadata处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleModifyMetadata(serviceUrl1, httpInstance.AppName, httpInstance.InstanceId, map[string]string{"hello": "world"})
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("ModifyMetadata请求方法:", response.Response.Request.Method)
-    fmt.Println("ModifyMetadata请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("ModifyMetadata响应状态:", response.StatusCode)
 }
 
 func TestQueryVipApps(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleQueryVipApps(serviceUrl1, instance.VipAddress)
-    ast.Nilf(response.Error, "QueryVipApps处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleQueryVipApps(serviceUrl1, httpInstance.VipAddress)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("QueryVipApps请求方法:", response.Response.Request.Method)
-    fmt.Println("QueryVipApps请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("QueryVipApps响应状态:", response.StatusCode)
-    fmt.Println("QueryVipApps响应报文:", response.Response.Body)
-    fmt.Println("QueryVipApps返回结果:", fmt.Sprintf("%#v", *response.Apps[0]))
+    ast.True(len(response.Apps) > 0)
 }
 
 func TestQuerySvipApps(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleQuerySvipApps(serviceUrl1, instance.SecureVipAddress)
-    ast.Nilf(response.Error, "QuerySvipApps处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleQuerySvipApps(serviceUrl1, httpInstance.SecureVipAddress)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("QuerySvipApps请求方法:", response.Response.Request.Method)
-    fmt.Println("QuerySvipApps请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("QuerySvipApps响应状态:", response.StatusCode)
-    fmt.Println("QuerySvipApps响应报文:", response.Response.Body)
-    fmt.Println("QuerySvipApps返回结果:", fmt.Sprintf("%#v", *response.Apps[0]))
+    ast.True(len(response.Apps) > 0)
 }
 
 func TestUnRegister(t *testing.T) {
     ast := assert.New(t)
-    response := httpClient.SimpleUnRegister(serviceUrl1, instance.AppName, instance.InstanceId)
-    ast.Nilf(response.Error, "UnRegister处理失败，失败原因：%s", response.Error)
+    response := httpClient.SimpleUnRegister(serviceUrl1, httpInstance.AppName, httpInstance.InstanceId)
+    ast.Nilf(response.Error, "%v", response.Error)
     ast.Equal(200, response.StatusCode)
-    fmt.Println("UnRegister请求方法:", response.Response.Request.Method)
-    fmt.Println("UnRegister请求URL:", response.Response.Request.RequestUrl)
-    fmt.Println("UnRegister响应状态:", response.StatusCode)
 }
